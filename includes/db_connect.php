4c83cafa7cc40e5c9a487c7e98a2d1670a0b2532
Muhammad Azka Ibrahim <aazzkkaa14@gmail.com>
2025-05-22 20:11:09 +0700

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -8,6 +8,8 @@ $password = '';
 try {
     // Create PDO connection
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
