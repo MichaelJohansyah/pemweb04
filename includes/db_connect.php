bff26eb08f29973fb705feed9e6504fad6139fe8
Muhammad Azka Ibrahim <aazzkkaa14@gmail.com>
2025-05-22 20:10:47 +0700

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -4,3 +4,12 @@ $host = 'localhost';
 $dbname = 'praktikum4';
 $username = 'root';
 $password = '';
+
+try {
+    // Create PDO connection
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?> 
